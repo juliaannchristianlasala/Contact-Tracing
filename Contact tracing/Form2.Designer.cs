fbca70262827d121f642c4003f83d07ecea0e80e
juliaannchristianlasala <jujualiah8@gmail.com>
2022-06-24 20:47:01 +0800

    All Data Design and Button

--- Contact tracing/Form2.Designer.cs
+++ Contact tracing/Form2.Designer.cs
@@ -29,21 +29,195 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.AIDLabel = new System.Windows.Forms.Label();
+            this.ContracingInfoLabel = new System.Windows.Forms.Label();
+            this.LineDesignOne = new System.Windows.Forms.Label();
+            this.LineDesignTwo = new System.Windows.Forms.Label();
+            this.LineDesignThree = new System.Windows.Forms.Label();
+            this.LineDesignFour = new System.Windows.Forms.Label();
+            this.InfoBgOne = new System.Windows.Forms.PictureBox();
+            this.InfoBgTwo = new System.Windows.Forms.PictureBox();
+            this.InfoBgThree = new System.Windows.Forms.PictureBox();
+            this.AllDataLabel = new System.Windows.Forms.Label();
+            this.ShowAllDatabutton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.InfoBgOne)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.InfoBgTwo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.InfoBgThree)).BeginInit();
             this.SuspendLayout();
+            // 
+            // AIDLabel
+            // 
+            this.AIDLabel.AutoSize = true;
+            this.AIDLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.AIDLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AIDLabel.Font = new System.Drawing.Font("Elephant", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.AIDLabel.ForeColor = System.Drawing.Color.Navy;
+            this.AIDLabel.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.AIDLabel.Location = new System.Drawing.Point(31, 28);
+            this.AIDLabel.Name = "AIDLabel";
+            this.AIDLabel.Size = new System.Drawing.Size(609, 42);
+            this.AIDLabel.TabIndex = 1;
+            this.AIDLabel.Text = "ADMIN INFORMATION DESK";
+            // 
+            // ContracingInfoLabel
+            // 
+            this.ContracingInfoLabel.AutoSize = true;
+            this.ContracingInfoLabel.BackColor = System.Drawing.Color.Transparent;
+            this.ContracingInfoLabel.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ContracingInfoLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ContracingInfoLabel.Location = new System.Drawing.Point(136, 72);
+            this.ContracingInfoLabel.Name = "ContracingInfoLabel";
+            this.ContracingInfoLabel.Size = new System.Drawing.Size(425, 24);
+            this.ContracingInfoLabel.TabIndex = 19;
+            this.ContracingInfoLabel.Text = "Contact Tracing Information of the Customers";
+            // 
+            // LineDesignOne
+            // 
+            this.LineDesignOne.AutoSize = true;
+            this.LineDesignOne.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LineDesignOne.Location = new System.Drawing.Point(21, 109);
+            this.LineDesignOne.Name = "LineDesignOne";
+            this.LineDesignOne.Size = new System.Drawing.Size(633, 20);
+            this.LineDesignOne.TabIndex = 20;
+            this.LineDesignOne.Text = "_________________________________________________________________________________" +
+    "_______________________";
+            // 
+            // LineDesignTwo
+            // 
+            this.LineDesignTwo.AutoSize = true;
+            this.LineDesignTwo.BackColor = System.Drawing.Color.Transparent;
+            this.LineDesignTwo.ForeColor = System.Drawing.Color.Navy;
+            this.LineDesignTwo.Location = new System.Drawing.Point(21, 597);
+            this.LineDesignTwo.Name = "LineDesignTwo";
+            this.LineDesignTwo.Size = new System.Drawing.Size(633, 20);
+            this.LineDesignTwo.TabIndex = 21;
+            this.LineDesignTwo.Text = "_________________________________________________________________________________" +
+    "_______________________";
+            // 
+            // LineDesignThree
+            // 
+            this.LineDesignThree.AutoSize = true;
+            this.LineDesignThree.BackColor = System.Drawing.Color.Transparent;
+            this.LineDesignThree.ForeColor = System.Drawing.Color.Navy;
+            this.LineDesignThree.Location = new System.Drawing.Point(21, 103);
+            this.LineDesignThree.Name = "LineDesignThree";
+            this.LineDesignThree.Size = new System.Drawing.Size(633, 20);
+            this.LineDesignThree.TabIndex = 22;
+            this.LineDesignThree.Text = "_________________________________________________________________________________" +
+    "_______________________";
+            // 
+            // LineDesignFour
+            // 
+            this.LineDesignFour.AutoSize = true;
+            this.LineDesignFour.BackColor = System.Drawing.Color.Transparent;
+            this.LineDesignFour.ForeColor = System.Drawing.Color.White;
+            this.LineDesignFour.Location = new System.Drawing.Point(21, 591);
+            this.LineDesignFour.Name = "LineDesignFour";
+            this.LineDesignFour.Size = new System.Drawing.Size(633, 20);
+            this.LineDesignFour.TabIndex = 23;
+            this.LineDesignFour.Text = "_________________________________________________________________________________" +
+    "_______________________";
+            // 
+            // InfoBgOne
+            // 
+            this.InfoBgOne.BackColor = System.Drawing.Color.Transparent;
+            this.InfoBgOne.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("InfoBgOne.BackgroundImage")));
+            this.InfoBgOne.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.InfoBgOne.ErrorImage = ((System.Drawing.Image)(resources.GetObject("InfoBgOne.ErrorImage")));
+            this.InfoBgOne.Location = new System.Drawing.Point(-8, 146);
+            this.InfoBgOne.Name = "InfoBgOne";
+            this.InfoBgOne.Size = new System.Drawing.Size(662, 129);
+            this.InfoBgOne.TabIndex = 24;
+            this.InfoBgOne.TabStop = false;
+            // 
+            // InfoBgTwo
+            // 
+            this.InfoBgTwo.BackColor = System.Drawing.Color.Transparent;
+            this.InfoBgTwo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("InfoBgTwo.BackgroundImage")));
+            this.InfoBgTwo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.InfoBgTwo.ErrorImage = ((System.Drawing.Image)(resources.GetObject("InfoBgTwo.ErrorImage")));
+            this.InfoBgTwo.Location = new System.Drawing.Point(20, 302);
+            this.InfoBgTwo.Name = "InfoBgTwo";
+            this.InfoBgTwo.Size = new System.Drawing.Size(662, 129);
+            this.InfoBgTwo.TabIndex = 25;
+            this.InfoBgTwo.TabStop = false;
+            // 
+            // InfoBgThree
+            // 
+            this.InfoBgThree.BackColor = System.Drawing.Color.Transparent;
+            this.InfoBgThree.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("InfoBgThree.BackgroundImage")));
+            this.InfoBgThree.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.InfoBgThree.ErrorImage = ((System.Drawing.Image)(resources.GetObject("InfoBgThree.ErrorImage")));
+            this.InfoBgThree.Location = new System.Drawing.Point(-10, 459);
+            this.InfoBgThree.Name = "InfoBgThree";
+            this.InfoBgThree.Size = new System.Drawing.Size(662, 129);
+            this.InfoBgThree.TabIndex = 26;
+            this.InfoBgThree.TabStop = false;
+            // 
+            // AllDataLabel
+            // 
+            this.AllDataLabel.AutoSize = true;
+            this.AllDataLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.AllDataLabel.Font = new System.Drawing.Font("Georgia", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.AllDataLabel.Location = new System.Drawing.Point(18, 167);
+            this.AllDataLabel.Name = "AllDataLabel";
+            this.AllDataLabel.Size = new System.Drawing.Size(614, 39);
+            this.AllDataLabel.TabIndex = 27;
+            this.AllDataLabel.Text = "All Data Gathered from Contact Tracing";
+            // 
+            // ShowAllDatabutton
+            // 
+            this.ShowAllDatabutton.BackColor = System.Drawing.Color.White;
+            this.ShowAllDatabutton.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ShowAllDatabutton.Location = new System.Drawing.Point(225, 223);
+            this.ShowAllDatabutton.Name = "ShowAllDatabutton";
+            this.ShowAllDatabutton.Size = new System.Drawing.Size(192, 34);
+            this.ShowAllDatabutton.TabIndex = 28;
+            this.ShowAllDatabutton.Text = "SHOW";
+            this.ShowAllDatabutton.UseVisualStyleBackColor = false;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(623, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(675, 638);
+            this.Controls.Add(this.ShowAllDatabutton);
+            this.Controls.Add(this.AllDataLabel);
+            this.Controls.Add(this.InfoBgThree);
+            this.Controls.Add(this.InfoBgTwo);
+            this.Controls.Add(this.InfoBgOne);
+            this.Controls.Add(this.LineDesignFour);
+            this.Controls.Add(this.LineDesignThree);
+            this.Controls.Add(this.LineDesignTwo);
+            this.Controls.Add(this.LineDesignOne);
+            this.Controls.Add(this.ContracingInfoLabel);
+            this.Controls.Add(this.AIDLabel);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Admin Information Desk";
+            ((System.ComponentModel.ISupportInitialize)(this.InfoBgOne)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.InfoBgTwo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.InfoBgThree)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label AIDLabel;
+        private Label ContracingInfoLabel;
+        private Label LineDesignOne;
+        private Label LineDesignTwo;
+        private Label LineDesignThree;
+        private Label LineDesignFour;
+        private PictureBox InfoBgOne;
+        private PictureBox InfoBgTwo;
+        private PictureBox InfoBgThree;
+        private Label AllDataLabel;
+        private Button ShowAllDatabutton;
     }
 }
